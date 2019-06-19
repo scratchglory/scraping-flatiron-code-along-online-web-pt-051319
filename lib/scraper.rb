3748f2f932de2f104197d0e162155137a28ec936
Adella <adellakenn@gmail.com>
2019-06-19 15:28:24 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,6 +25,7 @@ class Scraper
      new_course = Course.new()
      new_course.title = course.css(".post>h2").text
      new_course.schedule = course.css(".date").text
+     new_course.description = course.css(".post>h2").text
     end
   end
   
